4fa3be79b74e7226dce26d5dc4a9bdaf29d4e669
jirout@gmail.com <Jan.Jirout@equabank.cz>
2015-05-04 21:12:14 +0200

    test for inserting and deleting data in concurency environment

---
+++ src/test/java/com/coroptis/jblinktree/TreeDeleteConcurrencyTest.java
@@ -0,0 +1,79 @@
+package com.coroptis.jblinktree;
+
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * test verify that add and remove operations works in thread environment.
+ * 
+ * @author jajir
+ * 
+ */
+public class TreeDeleteConcurrencyTest extends TestCase {
+
+    private final Logger logger = LoggerFactory
+	    .getLogger(TreeDeleteConcurrencyTest.class);
+
+    private Tree tree;
+
+    private Random random;
+
+    @Test
+    public void testForThreadClash() throws Exception {
+	final int cycleCount = 10;
+	final int threadCount = 10;
+	final CountDownLatch doneLatch = new CountDownLatch(cycleCount
+		* threadCount);
+	final CountDownLatch startLatch = new CountDownLatch(1);
+
+	for (int i = 0; i < threadCount; ++i) {
+	    Runnable runner = new Executer(new Worker() {
+
+		@Override
+		public void doWork() {
+		    doWorkNow();
+		}
+	    }, startLatch, doneLatch, cycleCount);
+	    new Thread(runner, "TestThread" + i).start();
+	}
+
+	startLatch.countDown();
+	doneLatch.await(10, TimeUnit.SECONDS);
+	assertEquals("Some thread didn't finished work", 0,
+		doneLatch.getCount());
+	tree.verify();
+	logger.debug("I'm done!");
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+	super.setUp();
+	NodeStore nodeStore = new NodeStoreImpl();
+	tree = new Tree(2, nodeStore);
+	random = new Random();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+	tree = null;
+	super.tearDown();
+    }
+
+    void doWorkNow() {
+	Integer integer = random.nextInt(100) + 1;
+	logger.debug("inserting :" + integer);
+	if (integer % 2 == 0) {
+	    tree.insert(integer, integer);
+	} else {
+	    tree.remove(integer);
+	}
+    }
+
+}
