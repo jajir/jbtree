d8819af4fdcd9209a871e012e2af6d15bd9168d5
Jan Jirout <jirout@gmail.com>
2015-05-08 08:02:01 +0200

    header was corrected

--- src/main/java/com/coroptis/jblinktree/JblinktreeException.java
+++ src/main/java/com/coroptis/jblinktree/JblinktreeException.java
@@ -20,41 +20,24 @@ package com.coroptis.jblinktree;
  * #L%
  */
 
-/*
- * jblinktree
- * Copyright (C) 2015 coroptis
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-
 /**
  * Generic exception thrown by tree.
  * 
  * @author jajir
- *
+ * 
  */
 public class JblinktreeException extends RuntimeException {
 
-	/**
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public JblinktreeException(String message, Throwable cause) {
-		super(message, cause);
-	}
+    public JblinktreeException(String message, Throwable cause) {
+	super(message, cause);
+    }
 
-	public JblinktreeException(String message) {
-		super(message);
-	}
+    public JblinktreeException(String message) {
+	super(message);
+    }
 }
